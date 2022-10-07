3fad5cea9d35b16ea4df426e1f414b6cc18cf1eb
Thomas Parker <91673095+soloth@users.noreply.github.com>
2022-10-06 23:05:47 -0700

    move web app files to backend directory

--- backend/src/util/index.ts
+++ backend/src/util/index.ts
@@ -1,12 +1,12 @@
 const fs = require('fs');
 
 export function getJSON(file_name: string) {
-    let rawdata = fs.readFileSync("storage/" + file_name);
+    let rawdata = fs.readFileSync("backend/storage/" + file_name);
     return JSON.parse(rawdata)
 }
 
 export function writeJSON(file_name: string, data: any) {
-    fs.writeFileSync('storage/' + file_name, JSON.stringify(data, null, 4))
+    fs.writeFileSync('backend/storage/' + file_name, JSON.stringify(data, null, 4))
 
 }
 
