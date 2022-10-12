a20125d0c6af333cb811a98f3341380e8724afaf
Thomas Parker <91673095+soloth@users.noreply.github.com>
2022-10-11 18:31:31 -0700

    re-arrange dir

--- truffle/contracts/ProofOfConcept/Bridge/Subnet/IExposureSubnetBridge.sol
+++ truffle/contracts/ProofOfConcept/Bridge/Subnet/IExposureSubnetBridge.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-interface IExposureSubnetBridgeManager {
+interface IExposureSubnetBridge {
     function subnetAddresses(address mainnetAddress) external view returns (address subnetAddress);
 }
